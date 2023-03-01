f550627fbc57122ff8cac8a8eac6a32579ca8202
Michael Yuan <michael@secondstate.io>
2023-03-01 10:21:46 +0000

    Initial checkin
    Signed-off-by: Michael Yuan <michael@secondstate.io>

---
+++ README.txt
@@ -0,0 +1,7 @@
+# Robo Lawyer for your slack channel
+
+Build locally
+
+```
+cargo build --target wasm32-wasi --release
+```
---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+use slack_flows::{listen_to_channel, send_message_to_channel};
+use openai_flows::{CompletionRequest, create_completion};
+
+#[no_mangle]
+pub fn run() {
+    listen_to_channel("secondstate", "robo-lawyer", |sm| {
+        let cr = CompletionRequest {
+            prompt: "I want you to act as my legal advisor. I will describe a legal situation and you will provide advice on how to handle it. You should only reply with your advice, and nothing else. Do not write explanations. My request is \"".to_owned() + &sm.text + "\"",
+            ..Default::default()
+        };
+        let r = create_completion("Robo Lawyer", cr);
+        r.iter().for_each(|c| {
+            send_message_to_channel("secondstate", "robo-lawyer", c.to_string());
+        });
+    });
+}
