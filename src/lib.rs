2480f58fdf98a788a91644e31cbce0b8aad88d6a
Michael Yuan <michael@secondstate.io>
2023-03-01 12:18:15 +0000

    Change prompt
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use openai_flows::{CompletionRequest, create_completion};
 pub fn run() {
     listen_to_channel("secondstate", "robo-lawyer", |sm| {
         let cr = CompletionRequest {
-            prompt: "I want you to act as my legal advisor. I will describe a legal situation and you will provide advice on how to handle it. You should only reply with your advice, and nothing else. Do not write explanations. My request is \"".to_owned() + &sm.text + "\"",
+            prompt: "I want you to act as my legal advisor. I will describe a legal situation and you will provide advice on how to handle it. My question is \"".to_owned() + &sm.text + "\"",
             max_tokens: 2048,
             ..Default::default()
         };
