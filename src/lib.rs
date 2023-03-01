6c3cd29161096193f39a72479992535d210ec020
Michael Yuan <michael@secondstate.io>
2023-03-01 10:36:01 +0000

    Add max_tokens
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ pub fn run() {
     listen_to_channel("secondstate", "robo-lawyer", |sm| {
         let cr = CompletionRequest {
             prompt: "I want you to act as my legal advisor. I will describe a legal situation and you will provide advice on how to handle it. You should only reply with your advice, and nothing else. Do not write explanations. My request is \"".to_owned() + &sm.text + "\"",
+            max_tokens: 2048,
             ..Default::default()
         };
         let r = create_completion("RoboLawyer", cr);
