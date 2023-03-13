d7a683726e7d963d255672fe73ab920f7a3a9c2a
Michael Yuan <michael@secondstate.io>
2023-03-13 06:58:52 +0000

    Parameterize Slack target
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,34 @@
 use slack_flows::{listen_to_channel, send_message_to_channel};
 use openai_flows::{CompletionRequest, create_completion};
+use std::env;
+use dotenv::dotenv;
 
 #[no_mangle]
 pub fn run() {
-    listen_to_channel("secondstate", "robo-lawyer", |sm| {
+    dotenv().ok();
+    let workspace: String = match env::var("workspace") {
+        Err(_) => "secondstate".to_string(),
+        Ok(name) => name,
+    };
+
+    let channel: String = match env::var("channel") {
+        Err(_) => "robo-lawyer".to_string(),
+        Ok(name) => name,
+    };
+    let openai_key_name: String = match env::var("openai_key_name") {
+        Err(_) => "chatmichael".to_string(),
+        Ok(name) => name,
+    };
+
+    listen_to_channel(&workspace, &channel, |sm| {
         let cr = CompletionRequest {
             prompt: "I want you to act as my legal advisor. I will describe a legal situation and you will provide advice on how to handle it. My question is \"".to_owned() + &sm.text + "\"",
             max_tokens: 2048,
             ..Default::default()
         };
-        let r = create_completion("RoboLawyer", cr);
+        let r = create_completion(&openai_key_name, cr);
         r.iter().for_each(|c| {
-            send_message_to_channel("secondstate", "robo-lawyer", c.to_string());
+            send_message_to_channel(&workspace, &channel, c.to_string());
         });
     });
 }
